09dd1d04e0d41073ec537fbe7c94a1e3fd8087ea
Paulo <paulo.leite@academico.ifpb.edu.br>
2021-07-13 20:26:19 -0300

    :sparkles: feat - Default method

---
+++ br/com/DefaultMethods/Main.java
@@ -0,0 +1,4 @@
+package br.com.DefaultMethods;
+
+public class Main {
+}
---
+++ br/com/DefaultMethods/entities/BrasilInterestService.java
@@ -0,0 +1,14 @@
+package br.com.DefaultMethods.entities;
+
+public class BrasilInterestService implements InterestService {
+    private double interestRate;
+
+    public BrasilInterestService(double interestRate) {
+        this.interestRate = interestRate;
+    }
+
+    @Override
+    public double getInterestRate() {
+        return interestRate;
+    }
+}
